fc292eff1f0557152e1e7f5476ffad90a9f8d4e9
Cesar <44759176+Mixgyt@users.noreply.github.com>
2022-05-31 18:13:09 -0600

    se han añadido sprites y animaciones
    el sprite del robot se ha añadido y sus respectivas animaciones para moverse

--- Proyect-Bot/Assets/Scripts/Player/PlayerController.cs
+++ Proyect-Bot/Assets/Scripts/Player/PlayerController.cs
@@ -7,15 +7,19 @@ public class PlayerController : MonoBehaviour
    private Rigidbody2D rb2d;
 
    [Header("Movimiento")]
-   private float movimientoHorizontal = 0f;
    [SerializeField] private float velocidadDeMovimiento;
+   private float movimientoHorizontal = 0f;
    [Range(0,0.3f)][SerializeField] private float suavizadoDeMovimiento;
    private Vector3 velocidad = Vector3.zero;
    private bool mirandoDerecha = true;
+   
+   //Variables de Animación
+   private Animator botAnim;
 
     //Inicio del Script
    void Start(){
        rb2d = GetComponent<Rigidbody2D>();
+       botAnim = GetComponent<Animator>();
    }
 
     //Funcion repetida cada frame
@@ -32,6 +36,10 @@ public class PlayerController : MonoBehaviour
    private void Mover(float movimiento){
        Vector3 velocidadObjetivo = new Vector3(movimiento, rb2d.velocity.y);
        rb2d.velocity = Vector3.SmoothDamp(rb2d.velocity, velocidadObjetivo, ref velocidad, suavizadoDeMovimiento);
+
+        //Aqui se activa la animación si el personaje se mueve
+       if(movimientoHorizontal!=0){ botAnim.SetBool("walking",true); } 
+       else { botAnim.SetBool("walking",false); }
 
        if(movimiento>0 && !mirandoDerecha){
            Girar();
